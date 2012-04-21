fbda422c47b63257d370842784ac55e60c81643e
mesanovic.amer@gmail.com <mesanovic.amer@gmail.com@ca306dd2-e836-24c3-1740-06d6a085dac8>
2012-04-21 18:04:33 +0000

    Dodani Entiteti

--- Bobo Trans/DAL.cs
+++ Bobo Trans/DAL.cs
@@ -2,10 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace DAL
 {
-    public class DAL
+    public partial class DAL
     {
+        private string db, user, pass;
+        private static MySqlConnection con = null;
+
+        private static DAL instanca = null;
+
+        public static DAL Instanca
+        {
+            get { return (instanca == null) ? instanca = new DAL() : instanca; }
+        }
+
+        private DAL() { }
+        ~DAL() { terminirajKonekciju(); }
+
+
+        public void kreirajKonekciju(string server, string host, string db, string user, string pass)
+        {
+            if (con != null) return;
+            string connectionString = "server=" + server + ";user=" + user + ";pwd=" + pass + ";database=" + db;
+            con = new MySqlConnection(connectionString);
+
+            try
+            {
+                con.Open();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+
+        public void terminirajKonekciju()
+        {
+            try
+            {
+                if (con != null) con.Close();
+                con = null;
+            }
+            catch (Exception e) { throw e; }
+
+        }
+
+
+
     }
 }
